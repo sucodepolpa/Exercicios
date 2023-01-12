3458c3925f3b07a7ab6c132d4b245d2a698736d7
pietra <110630510+sucodepolpa@users.noreply.github.com>
2023-01-11 23:10:59 -0300

    Update for.c

--- for.c
+++ for.c
@@ -6,16 +6,16 @@ int main(void)
 {
     //
     int num, div;
-    int cont = 0; // armazena o numero de divisores de num
+    int count = 0; // armazena o numero de divisores de num
     //
     printf("digite um numero inteiro maior que 1 para saber se ele eh primo: ");
     scanf("%d", &num);
     for(div = 1; div <= num; div++){
         if(num % div == 0){
-            cont++;
+            count++;
         }
     }
-    if(cont == 2){
+    if(count == 2){
         printf("%d eh um numero primo.\n", num);
     }
     else{
